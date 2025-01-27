3c0f27994290ab32921009cafabfce6b8f3dada5
Riccardo Bean <86787032+riccardobean@users.noreply.github.com>
2025-01-27 01:10:54 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,13 +1,13 @@
 function detectDevice() {
     const screenWidth = window.innerWidth;
     if (screenWidth < 600) {
-        window.location.href = "https://riccardobean.github.io/portfolio/";
+        window.location.href = "https://riccardobean.github.io/portfolio/home";
     }
     else if (screenWidth >= 600 && screenWidth <= 1200) {
-        window.location.href = "https://riccardobean.github.io/portfolio/";
+        window.location.href = "https://riccardobean.github.io/portfolio/home";
     }
     else if (screenWidth > 1200){
-        window.location.href = "https://riccardobean.github.io/portfolio/";
+        window.location.href = "https://riccardobean.github.io/portfolio/home";
     }
 }
 
